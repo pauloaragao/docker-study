36a1bdbcc1612c2efd349156119d308a0af71821
Paulo Aragão <eng.pauloaragao@gmail.com>
2026-02-07 23:19:33 -0300

    refactor: comment out unused container listing code and force remove specific container

--- sdk-docker-python/index.py
+++ sdk-docker-python/index.py
@@ -8,7 +8,11 @@ client = docker.from_env()
 
 # client.containers.run("nginx:latest", detach=True, ports={"80/tcp": 8081})
 
-list_container = client.containers.list(all=True)
+#list_container = client.containers.list(all=True)
 
-for container in list_container:
-    print(container.name, container.status, container.image.tags, container.ports)
+#for container in list_container:
+#    print(container.name, container.id, container.status, container.image.tags, container.ports)
+
+container = client.containers.get("61c1807520d2f430f8c5e040a3bffd2f7a18d6fc34df32a848a8144219620c55")
+
+container.remove(force=True)
